7bc98c17d72cb75f71c91e6c509554cbd734c3b9
Korniichuk Alexandra <angel.win04@gmail.com>
2020-08-19 17:35:14 +0300

    hw8

--- m8.js
+++ m8.js
@@ -23,10 +23,51 @@ const generateItem = function (src, ds, alt) {
   return item;
 };
 
-const generateGallery = function (array, callback) {
-    let listGallery = array.forEach(elem => {
-        list.appendChild(callback(elem.preview, elem.original, elem.description));
-    });
-    return listGallery;
+const generateGallery = function (array, place) {
+  return array.map((elem) => {
+    let keys = Object.keys(elem);
+    const item = document.createElement("li");
+    const link = document.createElement("a");
+    link.classList.add("gallery__link");
+    link.setAttribute("href", "");
+    const img = document.createElement("img");
+    img.classList.add("gallery__image");
+    img.setAttribute("src", elem[keys[0]]);
+    img.setAttribute("data-source", elem[keys[1]]);
+    img.setAttribute("alt", elem[keys[2]]);
+
+    link.appendChild(img);
+    //   console.log(link);
+    item.appendChild(link);
+    place.appendChild(item);
+    // console.log(elem);
+    // callback(elem[keys[0]], elem[keys[1]], elem[keys[2]]);
+    // let a = callback(elem[keys[0]], elem[keys[1]], elem[keys[2]]);
+    // console.log(a);
+    // return list.insertAdjacentElement("afterbegin", a);
+  });
 };
-generateGallery(gallery, generateItem);
+generateGallery(gallery, list);
+
+// МОДАЛЬНОЕ ОКНО
+
+list.addEventListener("click", (event) => {
+  // console.log("click", event.target.nodeName);
+  // console.log("click", event.target.value);
+  toOpenModal(event);
+});
+
+function toOpenModal(event) {
+  if (event.target.nodeName === "IMG") {
+    div1.classList.add("is-open");
+    let path = event.target.getAttribute("data-source");
+    image.src = path;
+  }
+  return;
+}
+
+function toCloseModal() {
+  div1.classList.remove("is-open");
+  image.src = "";
+}
+button.addEventListener("click", toCloseModal);
